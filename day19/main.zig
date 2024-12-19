0a15bc2e68300ef7e37fabab7f3251c041684d11
Per Holmäng <per.holmang@gmail.com>
2024-12-19 14:34:24 +0100

    day19: part 2

--- day19/main.zig
+++ day19/main.zig
@@ -3,20 +3,40 @@ const aoc = @import("helpers.zig");
 const data = @embedFile("input.txt");
 const Allocator = std.mem.Allocator;
 
-fn matches_design(design: []const u8, patterns: [][]const u8) bool {
-    if (design.len == 0) {
+fn find_num_combinations(towel: []const u8, patterns: [][]const u8, memo: *std.StringHashMap(usize)) !usize {
+    if (towel.len == 0) return 1;
+
+    if (memo.get(towel)) |val| {
+        return val;
+    }
+
+    var ways: usize = 0;
+    for (patterns) |pattern| {
+        if (towel.len < pattern.len) {
+            continue;
+        }
+        if (std.mem.eql(u8, towel[0..pattern.len], pattern)) {
+            ways += try find_num_combinations(towel[pattern.len..], patterns, memo);
+        }
+    }
+
+    try memo.put(towel, ways);
+
+    return ways;
+}
+
+fn matches_towel(towel: []const u8, patterns: [][]const u8) bool {
+    if (towel.len == 0) {
         return true;
     }
     for (patterns) |pattern| {
-        if (design.len < pattern.len) {
+        if (towel.len < pattern.len) {
             continue;
         }
-        const idx = design.len - pattern.len;
-        //std.debug.print("Checking design: '{s}' {s}\n", .{ design[idx..], pattern });
-        if (std.mem.eql(u8, design[idx..], pattern)) {
-            const remaining_design = design[0..idx];
-            //std.debug.print("-> MATCH: '{s}' {s}\n", .{ design[idx..], pattern });
-            if (matches_design(remaining_design, patterns)) {
+        const idx = towel.len - pattern.len;
+        if (std.mem.eql(u8, towel[idx..], pattern)) {
+            const remaining_towel = towel[0..idx];
+            if (matches_towel(remaining_towel, patterns)) {
                 return true;
             }
         }
@@ -24,8 +44,8 @@ fn matches_design(design: []const u8, patterns: [][]const u8) bool {
     return false;
 }
 
-test "matches_design" {
-    //const design = "brwrr";
+test "matches_towel" {
+    //const towel = "brwrr";
     var patterns = std.ArrayList([]const u8).init(std.testing.allocator);
     try patterns.append("r");
     try patterns.append("wr");
@@ -37,7 +57,7 @@ test "matches_design" {
     try patterns.append("b");
     defer patterns.deinit();
 
-    const result = matches_design("bwurrg", patterns.items);
+    const result = matches_towel("bwurrg", patterns.items);
     try std.testing.expectEqual(true, result);
 }
 
@@ -45,33 +65,33 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
     const part = try aoc.getPart(alloc);
-    _ = part;
 
     const parsed_data = try parse(alloc, data);
     const patterns = parsed_data.patterns;
-    const designs = parsed_data.designs;
+    const towels = parsed_data.towels;
 
-    // std.debug.print("{s}\n", .{patterns});
+    var counter: usize = 0;
+    var memo = std.StringHashMap(usize).init(alloc);
 
-    var possible_designs: usize = 0;
-    for (designs) |design| {
-        if (matches_design(design, patterns)) {
-            std.debug.print("Matched design: {s}\n", .{design});
-            possible_designs += 1;
+    for (towels) |towel| {
+        if (part == aoc.Part.part2) {
+            counter += try find_num_combinations(towel, patterns, &memo);
+        } else {
+            counter += @intFromBool(matches_towel(towel, patterns));
         }
     }
-    std.debug.print("{d}\n", .{possible_designs});
+    std.debug.print("{d}\n", .{counter});
 }
 
 const ParsedData = struct {
     patterns: [][]const u8,
-    designs: [][]const u8,
+    towels: [][]const u8,
 };
 
 fn parse(alloc: Allocator, input: []const u8) !ParsedData {
     var reading_patterns: bool = true;
     var patterns = std.ArrayList([]const u8).init(alloc);
-    var designs = std.ArrayList([]const u8).init(alloc);
+    var towels = std.ArrayList([]const u8).init(alloc);
 
     var lines_it = std.mem.splitSequence(u8, input, "\n");
 
@@ -86,7 +106,7 @@ fn parse(alloc: Allocator, input: []const u8) !ParsedData {
                 try patterns.append(pattern);
             }
         } else {
-            try designs.append(line);
+            try towels.append(line);
         }
     }
 
@@ -94,7 +114,7 @@ fn parse(alloc: Allocator, input: []const u8) !ParsedData {
 
     std.mem.sort([]const u8, patterns_arr, {}, longer);
 
-    return ParsedData{ .patterns = patterns_arr, .designs = try designs.toOwnedSlice() };
+    return ParsedData{ .patterns = patterns_arr, .towels = try towels.toOwnedSlice() };
 }
 
 fn longer(_: void, u: []const u8, v: []const u8) bool {
